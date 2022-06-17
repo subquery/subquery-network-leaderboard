841fe6a22b5fa8aa83cc3892d0be03b45651cb9e
HZ <zhaihaohk@gmail.com>
2022-06-18 11:49:55 +1200

    Clear event handlers

--- src/mappings/indexerRegistry.ts
+++ src/mappings/indexerRegistry.ts
@@ -2,21 +2,13 @@
 // SPDX-License-Identifier: Apache-2.0
 
 import { AcalaEvmEvent } from '@subql/acala-evm-processor';
-import { EraManager__factory } from '@subql/contract-sdk';
 import {
   RegisterIndexerEvent,
   UnregisterIndexerEvent,
-  UpdateMetadataEvent,
 } from '@subql/contract-sdk/typechain/IndexerRegistry';
 import assert from 'assert';
 import { Indexer } from '../types';
-import FrontierEthProvider from './ethProvider';
-import {
-  bytesToIpfsCid,
-  upsertEraValue,
-  ERA_MANAGER_ADDRESS,
-  updateIndexerChallenges,
-} from './utils';
+import { updateIndexerChallenges } from './utils';
 
 /* Indexer Registry Handlers */
 export async function handleRegisterIndexer(
@@ -24,41 +16,17 @@ export async function handleRegisterIndexer(
 ): Promise<void> {
   logger.info('handleRegisterIndexer');
   assert(event.args, 'No event args');
-  const { indexer: indexerAddress, metadata } = event.args;
+  const { indexer: indexerAddress } = event.args;
 
   let indexer = await Indexer.get(indexerAddress);
-  const eraManager = EraManager__factory.connect(
-    ERA_MANAGER_ADDRESS,
-    new FrontierEthProvider()
-  );
-
-  // assert(!indexer, `Indexer (${indexerAddress}) already exists`);
-
   if (indexer) {
-    indexer.metadata = bytesToIpfsCid(metadata);
-    indexer.active = true;
-  } else {
     // Should not occurr. AddDelegation, SetCommissionRate events should happen first
     indexer = Indexer.create({
       id: indexerAddress,
-      metadata: bytesToIpfsCid(metadata),
-      totalStake: await upsertEraValue(eraManager, undefined, BigInt(0)),
-      // Set era to -1 as indicator to apply instantly in handleSetCommissionRate
-      commission: {
-        era: -1,
-        value: BigInt(0).toJSONType(),
-        valueAfter: BigInt(0).toJSONType(),
-      }, //await upsertEraValue(eraManager, undefined, BigInt(0)),
-      active: true,
-      demoProjectsIndexed: [],
       singleChallengePts: 0,
       singleChallenges: [],
     });
   }
-
-  /* WARNING, other events are emitted before this handler (AddDelegation, SetCommissionRate),
-   * their handlers are used to set their relevant values.
-   */
 
   await indexer.save();
 }
@@ -72,26 +40,9 @@ export async function handleUnregisterIndexer(
   const indexer = await Indexer.get(event.args.indexer);
   assert(indexer, `Expected indexer to exist: ${event.args.indexer}`);
 
-  indexer.active = false;
-  await indexer.save();
-
   await updateIndexerChallenges(
     indexer.id,
     'UNREGISTER_INDEXER',
     event.blockTimestamp
   );
-}
-
-export async function handleUpdateIndexerMetadata(
-  event: AcalaEvmEvent<UpdateMetadataEvent['args']>
-): Promise<void> {
-  logger.info('handleUpdateIndexerMetadata');
-  assert(event.args, 'No event args');
-  const address = event.args.indexer;
-
-  const indexer = await Indexer.get(address);
-  assert(indexer, `Expected indexer (${address}) to exist`);
-
-  indexer.metadata = bytesToIpfsCid(event.args.metadata);
-  await indexer.save();
 }
